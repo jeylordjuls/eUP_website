15eaddc6a6253058c308d73eb5790a41bf257038
jeylord julao <jeylordjulao@gmail.com>
2015-08-03 12:42:17 +0800

    latest

---
+++ env1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/var/www/html/IT_Web/env1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ static/js/imageswap.js
@@ -0,0 +1,10 @@
+ var sourceSwap = function () {
+        var $this = $(this);
+        var newSource = $this.data('alt-src');
+        $this.data('alt-src', $this.attr('src'));
+        $this.attr('src', newSource);
+    }
+
+    $(function () {
+        $('img.imagelink').hover(sourceSwap, sourceSwap);
+    });
